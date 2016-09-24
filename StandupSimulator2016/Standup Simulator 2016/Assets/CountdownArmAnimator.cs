121158d95fd9c7755c4c47451672c2311610492e
Viktor Pekar <vikpek@gmail.com>
2016-09-24 15:35:47 +0200

    - added rough countdown arm

---
+++ StandupSimulator2016/Standup Simulator 2016/Assets/CountdownArmAnimator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class CountdownArmAnimator : MonoBehaviour {
+
+	public Transform arm;
+
+	private const float secondsToDegrees = 180f / 120f;
+
+	void Start(){
+		StartCoroutine (StartCountdownWithSeconds(10));
+	}
+	public void InitateCountDown(int seconds){
+		StartCoroutine (StartCountdownWithSeconds(seconds));
+	}
+
+	IEnumerator StartCountdownWithSeconds(int seconds){
+		int restSeconds = seconds;
+		float tmp = 90f;
+		while (restSeconds > 0) {
+			DateTime time = DateTime.Now;
+			tmp -= secondsToDegrees;
+			arm.localRotation = Quaternion.Euler (0f, 0f, tmp);
+			restSeconds--;
+			yield return StartCoroutine(WaitAndPrint(1.0F));
+		}
+		yield return null;
+	}
+
+	IEnumerator WaitAndPrint(float waitTime) {
+		yield return new WaitForSeconds(waitTime);	
+	}
+}
