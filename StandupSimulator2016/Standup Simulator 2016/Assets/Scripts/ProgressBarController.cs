56153431bf7475fb9af3003f1377f05581cc8466
Viktor Pekar <vikpek@gmail.com>
2016-09-24 16:45:00 +0200

    - added quality bar

---
+++ StandupSimulator2016/Standup Simulator 2016/Assets/Scripts/ProgressBarController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ProgressBarController : MonoBehaviour {
+
+	public Image foregroundImage;
+	float currentQuality = 0f;
+
+	void Start(){
+		SetQualityToValue (0f);
+	}
+		
+	public void SetQualityToValue(float value){
+		currentQuality = value/100;
+		foregroundImage.fillAmount = currentQuality;
+	}
+
+	public void IncreaseQualityByValue(float value){
+		currentQuality += value/100;
+		foregroundImage.fillAmount = currentQuality;
+	}
+}
+
